056b1d8a7078e119984336fbf9e7246bd0574bd7
Baruch Frei <baruch@live.com>
2016-08-21 15:04:45 +0300

    add gulp test

--- gulpfile.js
+++ gulpfile.js
@@ -15,9 +15,9 @@ var knownOptions = {
 var options = minimist(process.argv.slice(2), knownOptions);
 
 gulp.task('test', () => 
-    gulp.src('test/test.js', {read: false})
+   return gulp.src('test/test.js', {read: false})
         // gulp-mocha needs filepaths so you can't have any plugins before it
-        .pipe(mocha({reporter: 'nyan'}))
+        .pipe(mocha({reporter: 'progress'}))
 );
 
 gulp.task('default', function () {
